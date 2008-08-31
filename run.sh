109413e1ce956d44e16ad88cfb21f6b5781111f5
Colin Walters <walters@src.gnome.org>
2008-08-31 19:21:52 +0000

    Tweak to be more useful
    svn path=/trunk/; revision=5

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-JGIRSRC=/src/build/gi/share/gitypelibs
+JGIRSRC=${INROOT_DIR}/share/girepository
 JARS=
 for j in ${JGIRSRC}/*.jar; do
   JARS="$JARS:$j"
 done
-CLASSPATH=$(build-classpath jna objectweb-asm/asm objectweb-asm/asm-{util,analysis,tree}):${JGIRSRC}/bin:${JARS}
+JARS="${JARS}:${INROOT_DIR}/share/java/jgir.jar"
+CLASSPATH=$(build-classpath jna objectweb-asm/asm objectweb-asm/asm-{util,analysis,tree}):${JARS}
 export CLASSPATH
 echo "CLASSPATH=$CLASSPATH"
-exec env GIREPOPATH=${JGIRSRC}/tests jhbuild run "$@"
+exec env "$@"
