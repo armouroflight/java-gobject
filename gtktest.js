8a5ee8b7a00e5c44aadc059916ce181c6062e109
Owen Taylor <otaylor@src.gnome.org>
2008-09-04 21:49:28 +0000

    Fix up gtktest.js to actually work run.sh: Print an error message if INROOT_DIR is not specified runjs.sh: New script for invoking Rhino on a javascript file with the right classpath gtktest.js: Add missing imports, connect handlers, make a guess at a better style
    svn path=/trunk/; revision=28

--- gtktest.js
+++ gtktest.js
@@ -1,9 +1,23 @@
-print("hello");
-var gtkPkg=Packages.org.gnome.gir.dynamic.Gtk;
-importPackage(gtkPkg);
-print(gtkPkg.Window);
-var win = Window(WindowType.TOPLEVEL);
-var button = Button("hello!");
+var gobject = Packages.org.gnome.gir.gobject.GObjectGlobals.init()
+var gtk = Packages.org.gnome.gir.dynamic.Gtk;
+
+gtk.GtkGlobals.init(null, null)
+
+var win = gtk.Window();
+
+var button = gtk.Button.newWithLabel("hello!");
+button.connect(gtk.Button.Clicked({
+	    onClicked: function () {
+		print("Hello");
+	    }}));
+
 win.add(button);
 win.showAll();
-GtkGlobals.main();
+
+win.connect(gtk.Widget.DeleteEvent({
+	    onDeleteEvent: function (event) {
+		gtk.GtkGlobals.mainQuit();
+		return true;
+	    }}));
+
+gtk.GtkGlobals.main();
--- runjs.sh
+++ runjs.sh
@@ -9,13 +9,20 @@ if [ x"$INROOT_DIR" = x ] ; then
     die "INROOT_DIR environment variable must be specified"
 fi
 
+SYSTEM_JARS="jna objectweb-asm/asm objectweb-asm/asm-{util,analysis,tree}"
+if [ x"$RHINO_JAR" = x ] ; then
+    JARS=
+    SYSTEM_JARS="$SYSTEM_JARS js.jar"
+else
+    JARS=$RHINO_JAR
+fi
+
 JGIRSRC=${INROOT_DIR}/share/girepository
-JARS=
 for j in ${JGIRSRC}/*.jar; do
   JARS="$JARS:$j"
 done
 JARS="${JARS}:${INROOT_DIR}/share/java/jgir.jar"
-CLASSPATH=$(build-classpath jna objectweb-asm/asm objectweb-asm/asm-{util,analysis,tree}):${JARS}
+CLASSPATH=$(build-classpath $SYSTEM_JARS):${JARS}
 export CLASSPATH
 echo "CLASSPATH=$CLASSPATH"
-exec env "$@"
+exec env /usr/bin/java org.mozilla.javascript.tools.shell.Main "$@"
